846990ab38a16e0138e6c621fef627edca3b0024
nikotatomir <niko.tatomir@gmail.com>
2022-05-04 16:52:45 +0200

    accounts/views.py modified such that register and login views decifer bewteen GET and POST requests.

--- accounts/views.py
+++ accounts/views.py
@@ -3,10 +3,18 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        # register user
+        pass
+    else:
+        return render(request, 'accounts/register.html')
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        # login user
+        pass
+    else:
+        return render(request, 'accounts/login.html')
 
 def logout(request):
     return redirect('index')
