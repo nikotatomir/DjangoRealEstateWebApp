a2485c80bf7ba8cefbb74fdc73d286f540553e8d
nikotatomir <niko.tatomir@gmail.com>
2022-05-07 15:33:53 +0200

    gmail email configuration added to btre/settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -141,3 +141,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# email content
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER='btrealestate10@gmail.com'
+EMAIL_HOST_PASSWORD = '10estaterealbt'
+EMAIL_USE_TLS=True
