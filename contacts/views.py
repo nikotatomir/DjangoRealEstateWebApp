3aef0891a0e380ea2805209920e1069c87d3cc29
nikotatomir <niko.tatomir@gmail.com>
2022-05-08 15:11:49 +0200

    email features completely removed.

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-# from django.core.mail import send_mail
 from .models import Contact
 
 # Create your views here.
@@ -27,15 +26,6 @@ def contact(request):
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
 
         contact.save()
-
-        #send email
-        # send_mail(
-        #     'Property Listing Inquiry',
-        #     'There has been an inquiry for ' + listing + '. Sign into the admin panel for more information.',
-        #     'btrealestate10@gmail.com',
-        #     [realtor_email, 'saurabhtmz@gmail.com', 'nikotatomir@gmail.com'],
-        #     fail_silently=False
-        # )
 
         messages.success(request, 'Your request has been submitted! A realtor will get back to you soon.')
 
